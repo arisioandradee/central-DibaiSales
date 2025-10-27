5b26671aa1a2fb6e67bc32c2253d23f288ac4bc3
Arisio Andrade <arisiosaf@gmail.com>
2025-10-27 09:38:21 -0300

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -17,14 +17,7 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:5173",
-        "http://127.0.0.1:5173",
-        "http://localhost:3000",
-        "http://127.0.0.1:3000",
-        "http://localhost:4173",
-        "http://127.0.0.1:4173",
-        "http://127.0.0.1:8000",
-        "http://localhost:8080"
+        "https://centraldibaisales.netlify.app/"
     ],
     allow_credentials=True, 
     allow_methods=["*"],
